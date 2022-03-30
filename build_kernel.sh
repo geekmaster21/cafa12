67c268c323fd6a11e7717cbb02be34575777256b
Maverick Leroux <maya-10@hotmail.ca>
2022-03-30 19:49:36 +0000

    Caf-A71

--- build_kernel.sh
+++ build_kernel.sh
@@ -18,20 +18,20 @@ export KBUILD_BUILD_HOST="OVH"
 CLANG_DIR="$KERNEL_DIR/clang"
 export PATH="$KERNEL_DIR/clang/bin:$PATH"
 export KBUILD_COMPILER_STRING="$("$CLANG_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-KERNEL_MAKE_ENV="DTC_EXT=$KERNEL_DIR/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
 
 make O=out ARCH=arm64 "$DEFCONFIG"
-make -j$(nproc --all) O=out \
-                    ARCH=arm64 \
-                    SUBARCH=arm64 \
-                    LD_LIBRARY_PATH="${CLANG_DIR}/lib:${LD_LIBRARY_PATH}" \
-                    CC=clang \
-                    AR=llvm-ar \
-                    NM=llvm-nm \
-                    OBJCOPY=llvm-objcopy \
-                    OBJDUMP=llvm-objdump \
-                    STRIP=llvm-strip \
-                    LD=ld.lld \
-                    CLANG_TRIPLE=aarch64-linux-gnu- \
-                    CROSS_COMPILE=aarch64-linux-gnu- \
-                    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+make O=out  ARCH=arm64 KERNEL_MAKE_ENV="DTC_EXT=/workspace/cafa12/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y" \
+            SUBARCH=arm64 \
+            LD_LIBRARY_PATH="${CLANG_DIR}/lib:${LD_LIBRARY_PATH}" \
+            CC=clang \
+            AR=llvm-ar \
+            NM=llvm-nm \
+            OBJCOPY=llvm-objcopy \
+            OBJDUMP=llvm-objdump \
+            STRIP=llvm-strip \
+            LD=ld.lld \
+            CLANG_TRIPLE=aarch64-linux-gnu- \
+            CROSS_COMPILE=aarch64-linux-gnu- \
+            CROSS_COMPILE_ARM32=arm-linux-gnueabi-
