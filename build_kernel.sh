28249c9f6a1a4a910e3e215dcaa4adf76a0c86a1
Maverick Leroux <78061003+geekmaster21@users.noreply.github.com>
2022-03-30 08:30:23 -0400

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,14 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Copyright ©2021 - 2022 XSans02
+# Docker Kernel Build Script
 
-export ARCH=arm64
-mkdir out
+# Installing some dep
+sudo apt-get install cpio libtinfo5
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
-CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
-
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a71_chn_hk_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
  
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+# Setup Environtment
+KERNEL_DIR=$PWD
+DEVICE=a71
+DEFCONFIG="$DEVICE"_defconfig
+export ARCH="arm64"
+export SUBARCH="arm64"
+export KBUILD_BUILD_USER="Geekmaster21"
+export KBUILD_BUILD_HOST="OVH"
+CLANG_DIR="$KERNEL_DIR/clang"
+export PATH="$KERNEL_DIR/clang/bin:$PATH"
+export KBUILD_COMPILER_STRING="$("$CLANG_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+KERNEL_MAKE_ENV="DTC_EXT=$KERNEL_DIR/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
+make O=out ARCH=arm64 "$DEFCONFIG"
+make -j$(nproc --all) O=out \
+                    ARCH=arm64 \
+                    SUBARCH=arm64 \
+                    LD_LIBRARY_PATH="${CLANG_DIR}/lib:${LD_LIBRARY_PATH}" \
+                    CC=clang \
+                    AR=llvm-ar \
+                    NM=llvm-nm \
+                    OBJCOPY=llvm-objcopy \
+                    OBJDUMP=llvm-objdump \
+                    STRIP=llvm-strip \
+                    LD=ld.lld \
+                    CLANG_TRIPLE=aarch64-linux-gnu- \
+                    CROSS_COMPILE=aarch64-linux-gnu- \
+                    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
