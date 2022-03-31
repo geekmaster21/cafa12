6cfe2c3616aa6d18c812137ce105edee624c9cbb
Maverick Leroux <maya-10@hotmail.ca>
2022-03-31 08:34:16 +0000

    TestKernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,7 +8,7 @@ sudo apt-get install cpio libtinfo5
 git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
  
 # Setup Environtment
-KERNEL_DIR=/home/ubuntu/android_tools/kernels/msm-4.14
+KERNEL_DIR=/workspace/cafa12
 DEVICE=a71
 DEFCONFIG="$DEVICE"_defconfig
 export ARCH="arm64"
@@ -18,11 +18,12 @@ export KBUILD_BUILD_HOST="OVH"
 CLANG_DIR="$KERNEL_DIR/clang"
 export PATH="$KERNEL_DIR/clang/bin:$PATH"
 export KBUILD_COMPILER_STRING="$("$CLANG_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-KERNEL_MAKE_ENV="DTC_EXT=$KERNEL_DIR/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+export DTC_EXT="$KERNEL_DIR/tools/dtc"
+export DTC_OVERLAY_TEST_EXT="$KERNEL_DIR/tools/ufdt_apply_overlay"
 
 make O=out ARCH=arm64 "$DEFCONFIG"
 
-make -j$(nproc --all) O=out  ARCH=arm64 $KERNEL_MAKE_ENV \
+make -j$(nproc --all) O=out  ARCH=arm64 \
             SUBARCH=arm64 \
             LD_LIBRARY_PATH="${CLANG_DIR}/lib:${LD_LIBRARY_PATH}" \
             CC=clang \
